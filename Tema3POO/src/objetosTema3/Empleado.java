b56ec4224fa74607f7a0241df56ee8205742813a
Proartema <141683511+Proartema@users.noreply.github.com>
2024-01-24 14:45:06 +0100

    Ejercicios 24/01

---
+++ Tema3POO/src/objetosTema3/Empleado.java
@@ -0,0 +1,69 @@
+package objetosTema3;
+
+public class Empleado {
+	
+	private int id;
+	private String nombre;
+	private String apellido;
+	private int salario;
+	
+		//constructor
+	public Empleado() {
+		
+	}
+	
+	public Empleado(int id,String nombre,String apellido,int salario) {
+		this.id=id;
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.salario=salario;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getSalario() {
+		return salario;
+	}
+
+	public void setSalario(int salario) {
+		this.salario = salario;
+	}
+	public int getSalarioAnual() {
+		return this.salario*12;
+	}
+	
+	public int AumentarSalario(int percent) {
+		double incremento = this.salario* (double) percent/100;
+		setSalario(this.salario + (int) Math.round(incremento));
+		return this.salario;
+	}
+
+	@Override
+	public String toString() {
+		
+		return "Empleado [id= " + id + ", Nombre= " + nombre + ", Apellido= " + apellido + ", Salario= " + salario + "]";
+		}
+
+}
