ad7593d5a63529b80cdfd41cd25ea5332d3b6702
Proartema <sandrosama2@gmail.com>
2024-01-30 21:25:18 +0100

    Ejercicios 7 y 8 hechos

---
+++ Tema3POO/src/ejercicios/esp/ejercicio3/Date.java
@@ -0,0 +1,73 @@
+package ejercicios.esp.ejercicio3;
+
+public class Date {
+	private int day;
+	private int month;
+	private int year;
+	
+	public Date() {
+		
+	}
+
+	public Date(int day, int month, int year) {
+		
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setDay(int day) {
+		if (day >=1 && day<=31 ) {
+		this.day = day;}
+		else {
+			throw new IllegalArgumentException("Number out of range");
+		}
+	}
+
+	public void setMonth(int month) {
+		if (month >=1 && month<=12 ) {
+		this.month = month;}
+		else {
+			throw new IllegalArgumentException("Number out of range");
+		}
+	}
+
+	public void setYear(int year) {
+		if (year >=1900 && year<=9999 ) {
+		this.year = year;}
+		else {
+			throw new IllegalArgumentException("Number out of range");
+		}
+	}
+	
+	public void setDate(int day, int month, int year) {
+		if (day >=1 && day<=31 && month >=1 && month<=12 && year>=1900 && year<=9999 ) {
+			this.day = day;
+			this.month=month;
+			this.year=year;}
+			else {
+				throw new IllegalArgumentException("Number out of range");
+			}
+	}
+	
+	@Override
+	public String toString() {
+		return day + " / " + month + " / " + year;
+	}
+	
+	
+	
+	
+}
---
+++ Tema3POO/src/ejercicios/esp/ejercicio3/Time.java
@@ -0,0 +1,80 @@
+package ejercicios.esp.ejercicio3;
+
+public class Time {
+	private int hour;
+	private int minute;
+	private int second;
+	
+	public Time() {
+		
+	}
+
+	public Time(int hour, int minute, int second) {
+		
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+	
+	public int getHour() {
+		return hour;
+	}
+
+	public int getMinute() {
+		return minute;
+	}
+
+	public int getSecond() {
+		return second;
+	}
+
+	public void setHour(int hour) {
+		if (hour >=0 && hour<=23 ) {
+			this.hour = hour;}
+			else {
+				throw new IllegalArgumentException("Number out of range");
+			}
+		}
+
+	public void setMinute(int minute) {
+		if (minute >=0 && minute<=59 ) {
+			this.minute = minute;}
+			else {
+				throw new IllegalArgumentException("Number out of range");
+			}
+		}
+
+	public void setSecond(int second) {
+		if (second >=0 && second<=59 ) {
+			this.second = second;}
+			else {
+				throw new IllegalArgumentException("Number out of range");
+			}
+		}
+	public void setTime(int hour, int minute, int second) {
+		
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+	public Time nextSecond() {
+		this.hour=hour;
+		this.minute=minute;
+		this.setSecond(second++);
+		
+		return  this;
+	}
+	public Time previousSecond() {
+		this.hour=hour;
+		this.minute=minute;
+		this.setSecond(second--);
+		
+		return  this;
+	}
+	@Override
+	public String toString() {
+		return hour + " : " + minute + " : " + second;
+	}
+	
+	
+}
