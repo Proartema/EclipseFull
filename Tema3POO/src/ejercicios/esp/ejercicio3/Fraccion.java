92912e2cf01dbce1f674a5d13b22d518c8a443f6
Proartema <141683511+Proartema@users.noreply.github.com>
2024-02-09 12:21:48 +0100

    Ejercicio Fracciones

--- Tema3POO/src/ejercicios/esp/ejercicio3/Fraccion.java
+++ Tema3POO/src/ejercicios/esp/ejercicio3/Fraccion.java
@@ -45,41 +45,84 @@ public class Fraccion{
 		this.denominador = denominador;
 	}
 	
-	public void Calcular_mcd () {
+	public int Calcular_mcd (int x, int y) {
+		int max = (x>=y)?x:y;
+		int min = (x<y)?x:y;
+		
+		while (min>0) {
+			int i= max%min;
+			max=min;
+			min=i;
+		}
+		return max;
+	}
+	public String Simplificar_fraccion(int numerador,int denominador) {
+		  int nuevoNumerador= numerador/Calcular_mcd(numerador, denominador);
+	        int nuevoDenominador= denominador/Calcular_mcd(numerador, denominador);
+
+	        return (nuevoNumerador+ (" / ")+ nuevoDenominador);
+	
+	}
+	public String Leer_fraccion() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Dime el numerador.");
+		this.numerador= input.nextInt();
+		System.out.println("Dime el denominador.");
+		this.denominador=input.nextInt();
+		
+		return Simplificar_fraccion(numerador,denominador);
+	}
+	public void Escribir_fraccion() {
+		
+		if (denominador>=1)
+			System.out.println(numerador + "/" + denominador);
+		else
+			System.out.println(numerador);
+		
 		
 	}
-	public void
-	public v Leer_fraccion() {
-		Scanner input = new Scanner(System.in);
+	
+	public String Sumar_fracciones(int x2, int y2) {
+		  int nuevoNumerador= numerador/Calcular_mcd(numerador, denominador);
+	        int nuevoDenominador= denominador/Calcular_mcd(numerador, denominador);
+	        int nuevoNumerador2= x2/Calcular_mcd(x2, y2); 
+	        int nuevoDenominador2= y2/Calcular_mcd(x2, y2); 
+	      int numeradorFinal = nuevoNumerador*nuevoDenominador2+ nuevoDenominador*nuevoNumerador2;
+	      int denominadorFinal = nuevoDenominador*nuevoDenominador2;
+	        return Simplificar_fraccion(numeradorFinal,denominadorFinal);
 	}
+	public String Restar_fracciones(int x2, int y2) {
+		  int nuevoNumerador= numerador/Calcular_mcd(numerador, denominador);
+	        int nuevoDenominador= denominador/Calcular_mcd(numerador, denominador);
+	        int nuevoNumerador2= x2/Calcular_mcd(x2, y2); 
+	        int nuevoDenominador2= y2/Calcular_mcd(x2, y2); 
+	      int numeradorFinal = nuevoNumerador*nuevoDenominador2 - nuevoDenominador*nuevoNumerador2;
+	      int denominadorFinal = nuevoDenominador*nuevoDenominador2;
+	        return Simplificar_fraccion(numeradorFinal,denominadorFinal);
+	}
+	public String Multiplicar_fracciones(int x2, int y2) {
+		  int nuevoNumerador= numerador/Calcular_mcd(numerador, denominador);
+	        int nuevoDenominador= denominador/Calcular_mcd(numerador, denominador);
+	        int nuevoNumerador2= x2/Calcular_mcd(x2, y2); 
+	        int nuevoDenominador2= y2/Calcular_mcd(x2, y2); 
+	      int numeradorFinal = nuevoNumerador*nuevoNumerador2;
+	      int denominadorFinal = nuevoDenominador*nuevoDenominador2;
+	        return Simplificar_fraccion(numeradorFinal,denominadorFinal);
+	}
+	public String Dividir_fracciones(int x2, int y2) {
+		  int nuevoNumerador= numerador/Calcular_mcd(numerador, denominador);
+	        int nuevoDenominador= denominador/Calcular_mcd(numerador, denominador);
+	        int nuevoNumerador2= x2/Calcular_mcd(x2, y2); 
+	        int nuevoDenominador2= y2/Calcular_mcd(x2, y2); 
+	      int numeradorFinal = nuevoNumerador*nuevoDenominador2;
+	      int denominadorFinal = nuevoDenominador*nuevoNumerador2;
+	        return Simplificar_fraccion(numeradorFinal,denominadorFinal);
+	}
+	
 
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public numerico sumar(numerico numero) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public numerico restar() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public numerico multiplicar() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public numerico dividir() {
 		// TODO Auto-generated method stub
 		return null;
 	}
